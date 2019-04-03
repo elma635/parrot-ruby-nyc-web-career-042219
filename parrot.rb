701fa794dc8e79d8b83f965356265d19ab2f426e
Elma Rahman <elmarahman635@gmail.com>
2019-04-03 00:00:01 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -3,26 +3,3 @@
 def parrot  (default= Squawk!)
   puts "parrot = #{default}"
 end
-
-describe '#parrot' do
-  it 'should output "Squawk!" to the terminal when called without any arguments' do
-    expect($stdout).to receive(:puts).with("Squawk!")
-    parrot
-  end
-
-  it 'should return the default phrase, "Squawk!" when called without any arguments' do
-    phrase = parrot
-    expect(phrase).to eq("Squawk!")
-  end
-
-  it 'should output the given phrase when called with an argument' do
-    expect($stdout).to receive(:puts).with("Pretty bird!")
-    parrot("Pretty bird!")
-  end
-
-  it 'should return the given phrase when called with an argument' do
-    phrase = parrot("Pretty bird!")
-
-    expect(phrase).to eq("Pretty bird!")
-  end
-end
